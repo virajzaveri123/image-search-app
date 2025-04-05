ff30f91914d94ce7c6910706e574cfdfa637c134
Viraj Virang Zaveri <53605568+virajzaveri123@users.noreply.github.com>
2025-04-05 20:55:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -138,4 +138,5 @@ def index():
     return render_template("index.html", results=results, model=model_used)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
